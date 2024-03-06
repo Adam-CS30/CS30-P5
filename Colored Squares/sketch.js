d7cd23c6bcac3bb2650609d27e33be72e7c5d11e
Adam-CS30 <7001053@spsd.sk.ca>
2024-03-06 14:18:34 -0600

    Added Color
    - Random color generation

--- Colored Squares/sketch.js
+++ Colored Squares/sketch.js
@@ -10,10 +10,27 @@ function setup() {
 }
 
 function mousePressed(){
-  let size = 20
+  let R = random(50,255);
+  let G = random(50,255);
+  let B = random(50,255);
+  let size = 20;
+  let n = 10;
+  let squares = pow(((width - width%size) / size),2);
+  
   for (let x = size/2; x < width; x+=size){
     for (let y = size/2; y < height; y+=size){
-      fill(random(0,255))
+      R = random(R-n, R+n);
+      G = random(G-n, G+n);
+      B = random(B-n, B+n);
+      if (R < 0){R=0;}
+      if (G < 0){G=0;}
+      if (B < 0){B=0;}
+      if (R > 255){R=255;}
+      if (G > 255){G=255;}
+      if (B > 255){B=255;}
+      
+      fill(R,G,B)
+
       square(x, y, size)
     }
   }
