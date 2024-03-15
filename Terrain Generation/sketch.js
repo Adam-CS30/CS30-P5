94df93972310726b5c65d05ac595e3259091d6c5
Adam-CS30 <7001053@spsd.sk.ca>
2024-03-15 15:20:55 -0600

    Average band and panning
    - added band on the average height
    - the scene now pans

--- Terrain Generation/sketch.js
+++ Terrain Generation/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let w = 10;
+let time = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -25,23 +26,40 @@ function keyPressed(){
 
 function generateTerrain(){
   fill(0);
-  let t = 0;
+  let time_added = 0;
   let peakY = height;
-  let peakX;
+  let peakX = 0;
+  let total_height = 0;
+  let heights = [];
 
   for (let x = 0; x < width; x+=w){
-    let y = height - map(noise(t), 0, 1, height*0.8, height*0.2);
+    let y = height - map(noise(time), 0, 1, height*0.2, height*0.8);
     rect(x, height, x+w, y);
+    heights.push(y);
+    time += 0.01;
+    time_added += 0.01;
 
     if (y < peakY){
       peakX = x + w/2;
       peakY = y;
+      print(peakX);
     }
-
-    t += 0.01;
   }
 
+  for (y of heights){total_height += y;}
+  //if (peakX > width){print(peakX)}
+  avg_height = total_height/heights.length;
+
+ if (peakX < width){peakX-=w/2}
+  drawBand(avg_height);
   drawFlag(peakX, peakY);
+  time -= time_added*0.99
+}
+
+function drawBand(y){
+  fill(255,50,50,100);
+  rect(0,y-5,width,y+10);
+  fill(0);
 }
 
 function drawFlag(x, y){
@@ -49,6 +67,6 @@ function drawFlag(x, y){
   strokeWeight(1);
   fill(150);
   line(x,y,x,y-tall);
-  triangle(x, y-tall, x, y-tall/1.8, x+tall/3, y-(tall*2/3.6))
+  triangle(x, y-tall, x, y-tall/1.8, x+tall/2.5, y-(tall+tall/1.8)/2);
   strokeWeight(0);
 }
