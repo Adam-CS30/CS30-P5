46595533447dec74dca79685403bf4362cf61bb7
Adam-CS30 <7001053@spsd.sk.ca>
2024-03-27 15:22:42 -0600

    Worked on Art Replication
    - Created sin function
    - Can draw 99 lines

--- Assignments/Art Replication/sketch.js
+++ Assignments/Art Replication/sketch.js
@@ -6,9 +6,15 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  strokeWeight(1.5);
+  background(220);
+  for (let y = 0; y < 99; y++){sinusoid(y*3+300)}
 }
 
-function draw() {
-  background(220);
-  //print(sin(3));
-}
+function sinusoid(y){
+  for (let x = width*0.1; x <= width*0.9; x++){
+    let sinx = map(x,width*0.1,width*0.9,0,5);
+    let B = map(x,width*0.1,width*0.9,0,5);
+    point(x,y+20*sin(B*(sinx+1.5)));
+  }
+ }
--- Lessons and Examples/Snake/sketch.js
+++ Lessons and Examples/Snake/sketch.js
@@ -3,20 +3,19 @@
 // March 22, 2024
 // Replicate the original snake game
 
-let x = [], y = [], speed = 1, segments = 5, directionX = 20, directionY = 0, dead = false, size = 20;
+let x = [], y = [], speed = 1, segments = 5, directionX = 20, directionY = 0, tempDirectionX = 20, tempDirectionY = 0, dead = false, size = 20;
 
 function setup() {
   createCanvas(600, 600);
   noStroke();
-  rectMode(CENTER);
+  //rectMode(CENTER);
   frameRate(speed);
-  for (let i = 0; i < segments; i++)
-  x.push(width/2 - size);
-  x.push(width/2);
-  x.push(width/2 + size);
-  y.push(height/2);
-  y.push(height/2);
-  y.push(height/2);
+  for (let i = 0; i < segments; i++){
+    if (i < (segments-1)/2){x.push(width/2 - size*(((segments-1)/2)-i));}
+    else if (i === (segments-1)/2){x.push(width/2);}
+    else{x.push(width/2 + size*((segments-1)/2-(segments-(i+1))));}
+    y.push(height/2);
+  }
 }
 
 function draw() {
@@ -30,14 +29,16 @@ function snake(){
   for (let i = 0; i < segments; i++){
     if (i === x.length-1){
       fill(255,0,0);
-      square(x[i],y[i],size-2);
+      square(x[i]+1,y[i]+1,size-2);
     }
-    else {square(x[i],y[i],size-2);}
+    else {square(x[i]+1,y[i]+1,size-2);}
     fill(210)
   }
 }
 
 function updateBody(){
+  directionX = tempDirectionX;
+  directionY = tempDirectionY;
   x.push(x[segments-1] + directionX);
   y.push(y[segments-1]  + directionY);
   x.shift()
@@ -45,8 +46,8 @@ function updateBody(){
 }
 
 function keyPressed(){
-  if (keyCode === LEFT_ARROW && directionX === 0){directionX = -20; directionY = 0;}
-  else if (keyCode === RIGHT_ARROW && directionX === 0){directionX = 20; directionY = 0;}
-  else if (keyCode === UP_ARROW && directionY === 0){directionX = 0; directionY = -20;}
-  else if (keyCode === DOWN_ARROW && directionY === 0){directionX = 0; directionY = 20;}
+  if (keyCode === LEFT_ARROW && (directionX === 0 || tempDirectionX === 0)){tempDirectionX = -20; tempDirectionY = 0;}
+  else if (keyCode === RIGHT_ARROW && (directionX === 0 || tempDirectionX === 0)){tempDirectionX = 20; tempDirectionY = 0;}
+  else if (keyCode === UP_ARROW && (directionY === 0 || tempDirectionY === 0)){tempDirectionX = 0; tempDirectionY = -20;}
+  else if (keyCode === DOWN_ARROW && directionY === 0){tempDirectionX = 0; tempDirectionY = 20;}
 }
