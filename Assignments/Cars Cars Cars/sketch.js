8dc28bc0bb52f861c31fb51dba2fa9fe5805cae8
Adam-CS30 <7001053@spsd.sk.ca>
2024-04-18 15:21:54 -0600

    Added trafficLight class

--- Assignments/Cars Cars Cars/sketch.js
+++ Assignments/Cars Cars Cars/sketch.js
@@ -3,18 +3,21 @@
 // April 15, 2024
 // 
 
-let westbound = [], eastbound = [];
+let westbound = [], eastbound = [], trafficLight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  ellipseMode(CENTER);
   for (let i = 0; i < 20; i++){eastbound.push(new Vehicle(0, random(height*0.52, height*0.63)));}
   for (let i = 0; i < 20; i++){westbound.push(new Vehicle(width, random(height*0.37, height*0.48)));}
+  trafficLight = new TrafficLight();
 }
 
 function draw() {
   background(220);
   drawRoad();
+  trafficLight.action();
   for (let v of eastbound){v.action();}
   for (let v of westbound){v.action();}
 }
@@ -26,9 +29,18 @@ function drawRoad(){
   for (let x = 0; x <= width; x+=50){rect(x, height/2, 30, 5);}
 }
 
+function mousePressed(){
+  if (mouseButton === LEFT){
+    if (keyCode === SHIFT){westbound.push(new Vehicle(width, random(height*0.37, height*0.48)));}
+    else{eastbound.push(new Vehicle(0, random(height*0.52, height*0.63)));}
+  }
+}
+
+function keyPressed(){trafficLight.switchLight()}
+
 class Vehicle{
   constructor(x, y){
-    this.type = int(random(2)); this.x = width-20; this.y = y; this.xSpeed = 1; this.color = color(random(255),random(255),random(255))
+    this.type = int(random(2)); this.x = x; this.y = y; this.xSpeed = int(random(5,9)); this.color = color(random(255),random(255),random(255))
     if (this.y < height/2){this.reflect = -1}
     else {this.reflect = 1}
   }
@@ -52,7 +64,8 @@ class Vehicle{
 
   move(){
     this.x += this.xSpeed*this.reflect;
-    if (this.x > width){this.x=0}
+    if (this.x > width && this.reflect === 1){this.x=-50}
+    else if (this.x < 0 && this.reflect === -1){this.x=width+50}
   }
 
   speedUp(){if (this.xSpeed < 15){this.xSpeed++};}
@@ -62,9 +75,44 @@ class Vehicle{
   action(){
     let r = random();
     this.move();
-    //if (r < 0.01){this.speedUp();}
-    //else if (r < 0.02){this.speedDown();}
+    if (r < 0.01){this.speedUp();}
+    else if (r < 0.02){this.speedDown();}
     if (random() < 0.01){this.changeColor();}
+    this.display();
+  }
+}
+
+class TrafficLight{
+  constructor(){
+    this.light = 0; this.timer = -120;
+  }
+
+  switchLight(){
+    if (key === ' '){
+      this.light = 1;
+      this.timer = frameCount;
+    }
+  }
+
+  display(){
+    fill(60);
+    rect(width/2, height/4, 400, 120);
+
+    if (this.light === 0){fill(100,20,20)}
+    else{fill(255,50,50)}
+
+    circle(width/2 - 125, height/4, 100);
+    fill(100, 50, 0);
+    circle(width/2, height/4, 100);
+
+    if (this.light === 1){fill(20,100,20)}
+    else{fill(50,200,50)}
+
+    circle(width/2 + 125, height/4, 100);
+  }
+
+  action(){
+    if (frameCount-this.timer >= 120){this.light = 0;}
     this.display();
   }
 }
