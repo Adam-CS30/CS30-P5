b033d731bd31689ca502d1fdf80ee36d77dadefb
Adam-CS30 <7001053@spsd.sk.ca>
2024-04-29 15:51:05 -0600

    Minor changes to grid game

--- Lessons and Examples/2D Arrays Example/sketch.js
+++ Lessons and Examples/2D Arrays Example/sketch.js
@@ -2,15 +2,22 @@
 // Adam Abouelela
 // April 23, 2024
 
+// SQUARE_SIZE is the size of each tile, while NUM_ROWS and NUM_COLS are the number of rows and columns.
 const SQUARE_SIZE = 50; const NUM_ROWS = 6; const NUM_COLS = 6;
-let grid = [], squareMode = false, win = 0, n = 0;
+
+let grid = []; // grid will be a 2D array that stores the states of all tiles.
+let squareMode = false; // squareMode is a boolean that stores whether or not the flip mode should be a square(true) or cross(false).
+let win = false; // Stores whether the player has won.
+let n = 0; // n is used in the win animations.
 
 function setup() {
   createCanvas(NUM_COLS * SQUARE_SIZE, NUM_ROWS * SQUARE_SIZE);
+
+  // Creates a 2D array where each item is randomly either 0 or 255.
   for (let y = 0; y < NUM_ROWS; y++){
     grid.push([]);
     for (let x = 0; x < NUM_COLS; x++){
-      grid[y].push(int(random(1))*255);
+      grid[y].push(int(random(2))*255);
     }
   }
 }
@@ -18,15 +25,20 @@ function setup() {
 function draw() {
   background(220);
   
+  // If the game was won, stroke is turned off.
   if (win){noStroke();}
+
+  // Draws the grid.
   drawGrid();
+  // Creates an overlay on top of the grid to show which tiles would be flipped.
   if (!win){tileOverlay(int(constrain(mouseX/SQUARE_SIZE, 0, NUM_COLS-1)), int(constrain(mouseY/SQUARE_SIZE, 0, NUM_ROWS-1)));}
   
+  // If the game was won, display the win animation.
   if (win){animateWin();}
 }
 
 function drawGrid(){
-  // Read data from 2D array (grid), and use the numbers there to set the color for squares which are arranged in a grid fashion.
+  // Draw the grid by reading the values in the 2D array "grid" and using the numbers there as the color for each square on the grid.
   for (let y = 0; y < NUM_ROWS; y++){
     for(let x = 0; x < NUM_COLS; x++){
       fill(grid[y][x]);
@@ -40,17 +52,18 @@ function animateWin(){
   // If the filled board was white:
   if (grid[0][0] === 255){
 
-    // The borders between the tiles is removed and replaced by fake ones that get shorter until they disappear.
+    // The borders between the tiles is removed and replaced by fake lines that get shorter until they disappear.
+    // Animation stops when the lines have disappeared.
     stroke(0);
     if (n <= width/2){
       for (let y = 0; y <= NUM_ROWS; y++){
         for(let x = 0; x <= NUM_COLS; x++){line(x*SQUARE_SIZE, 0+n, x*SQUARE_SIZE, height-n);}
         line(0+n, y*SQUARE_SIZE, width-n, y*SQUARE_SIZE);
       }
-      n += 3;
+      n += 3; // The length of each line gets shorter by n*2 each frame.
     }
-    // When the animation has played display 'YOU WIN!'.
-    else{textWin();}
+    
+    else{textWin();} // When the animation has stopped display 'YOU WIN!'.
   }
   
   // If the filled board was black:
@@ -60,8 +73,8 @@ function animateWin(){
     fill(255);
     rect(0, height/2-n/2, width, n);
     textWin();
-    // The height of the rectangle grows exponentially.
-    if (n <= height){n += 0.01*pow(n, 1.3)+1;}
+    
+    if (n <= height){n += 0.01*pow(n, 1.3)+1;} // The height of the rectangle 'n' grows exponentially until it spans the whole height of the screen.
   }
 }
 
@@ -109,6 +122,7 @@ function flipTiles(x, y){
   }
 }
 
+// Creates an overlay on the tiles that will be flipped based on the 
 function tileOverlay(x, y){
   fill(50, 220, 100, 150);
 
@@ -127,10 +141,12 @@ function tileOverlay(x, y){
   }
 }
 
+// Checks to see if every tile's color is equal to the first tile. If so, return true.
 function checkWin(){
   let tile = grid[0][0];
   for (let y = 0; y < NUM_ROWS; y++){
     for(let x = 0; x < NUM_COLS; x++){
+      // If any tile doesn't match the first tile, return false.
       if (tile !== grid[y][x]){
         return false;
       }
@@ -140,12 +156,16 @@ function checkWin(){
 }
 
 function mousePressed(){
+  // If the mouse is on the canvas while a left click occurs and the game hasn't been won, certain tiles will flip color.
   if (mouseButton === LEFT && mouseX <= width && mouseY <= height && !win){
+    // 
     flipTiles(int(constrain(mouseX/SQUARE_SIZE, 0, NUM_COLS-1)), int(constrain(mouseY/SQUARE_SIZE, 0, NUM_ROWS-1)));
+    // Check to see if the flip caused the game to be won.
     win = checkWin();
   }
 }
 
+// When the spacebar is pressed toggle the flip mode between a square and a cross.
 function keyPressed(){
   if (key === ' '){
     if (squareMode){squareMode = false;}
