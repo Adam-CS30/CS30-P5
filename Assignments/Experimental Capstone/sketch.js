d820a348c1579f65feaeb8542f355e0a8085b508
Adam-CS30 <7001053@spsd.sk.ca>
2024-05-09 15:50:09 -0600

    Multiple files and children classes

--- Assignments/Experimental Capstone/sketch.js
+++ Assignments/Experimental Capstone/sketch.js
@@ -24,7 +24,8 @@ class Player{
     this.acc = createVector(0, 0);
     this.keybinds = {'dash':[32, SHIFT], 'left':[LEFT_ARROW, 65], 'right':[RIGHT_ARROW, 68], 'up':[UP_ARROW, 87], 'down':[DOWN_ARROW, 83]}
     this.dashing = false;
-    this.dashes = 1
+    this.movespeed = 1;
+    this.dashes = 3;
     this.alive = true;
   }
 
@@ -40,11 +41,11 @@ class Player{
   }
 
   move(action){
-    if (action === 'dash' && !this.dashing) {this.vel.x = 0; this.dashing = true;}
-    if (action === 'left') {this.vel.x -= 1.5;}
-    if (action === 'right') {this.vel.x += 1.5;}
-    if (action === 'up') {this.vel.y -= 1.5;}
-    if (action === 'down') {this.vel.y += 1.5 * ((this.dashing)?30:1);}
+    if (action === 'dash' && this.dashes > 0) {this.vel.x = 0; this.dashing = true; this.movespeed = 30; this.dashes--;}
+    if (action === 'left') {this.vel.x -= this.movespeed;}
+    if (action === 'right') {this.vel.x += this.movespeed;}
+    if (action === 'up') {this.vel.y -= this.movespeed;}
+    if (action === 'down') {this.vel.y += this.movespeed;}
   }
 
   display(){
@@ -55,10 +56,11 @@ class Player{
     this.acc.x = 0;
     this.acc.y = 0;
 
-    //noStroke();
-    this.dashing = false
-    print(this.vel.x)
-    fill(100, 240, 100);
+    noStroke();
+    this.dashing = false;
+    this.movespeed = 1;
+
+    fill(100, 200, 100);
     rect(this.pos.x, this.pos.y, 20, 40);
   }
 }
---
+++ Assignments/Testing/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(1);
+}
+
+function draw() {
+  background(220);
+}
+
+function keyPressed(){
+  print(key);
+}
