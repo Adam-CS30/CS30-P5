a8b8886f28b1d9c8cf2171d388a4860b15f2a55e
Adam-CS30 <7001053@spsd.sk.ca>
2024-05-23 15:39:42 -0600

    Worked on player movement mechanics and abilities.

--- Assignments/Experimental Capstone/sketch.js
+++ Assignments/Experimental Capstone/sketch.js
@@ -3,12 +3,12 @@
 // May 2, 2024
 // Experimental capstone idea
 
-let player, screen1, scaling = 2.5, paused = false;
+let player, screen1, scaling = 2.7, paused = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   player = new Player(2, height/(2*scaling));
-  screen1 = [new NeutralTerrain(0, 5, 1, 1), new NeutralTerrain(2, 5, 1, 1), new NeutralTerrain(4, 5, 1, 1)];
+  screen1 = [new NeutralTerrain(0, 5, 9, 9), new NeutralTerrain(2, 4, 1, 1), new NeutralTerrain(4, 4, 1, 1)];
 }
 
 function draw() {
@@ -38,37 +38,68 @@ function pauseScreen(){
 }
 
 function keyPressed(){
-  if (key === ' ' && player.dashes > 0) {player.dash();}
-
-  else if (keyCode === ESCAPE){
-    print('pause triggered')
+  if (keyCode === ESCAPE){
+    print('pause triggered');
     if (paused){paused = false;}
     else{paused = true;}
   }
+
+  else{player.checkAbility();}
 }
 
 class Player{
-  // 9 x 18 hitbox
+  // 9 x 17 hitbox
   constructor(x, y){
     this.realPos = createVector(x, y);
     this.oldPos = createVector(x, y);
     this.pos = createVector(x, y);
+    this.moveVel = createVector(0, 0);
+    this.naturalVel = createVector(0, 0);
 
-    this.vel = createVector(0, 0);
-    this.keybinds = {'left':[LEFT_ARROW, 65], 'right':[RIGHT_ARROW, 68], 'up':[UP_ARROW, 87], 'down':[DOWN_ARROW, 83]}
+    this.keybinds = {'left':[LEFT_ARROW, 65], 'right':[RIGHT_ARROW, 68], 'up':[UP_ARROW, 87], 'down':[DOWN_ARROW, 83]};
+    this.specialKeys = {'dash':['j', 'e'], 'jump':['k', ' ']};
+    this.toMove = [];
+
+    this.triggerJump = false;
+    this.triggerDash = false;
     this.dashing = false;
+
 	  this.stationary = true;
-    this.facing = 1;
-    this.regularMovespeed = 1;
-    this.dashes = 1;
+    this.grounded = true;
     this.alive = true;
+
+    this.regularMovespeed = 1;
+    this.facing = 1;
+    this.dashes = 2;
   }
 
-  determineMovement(){
+  checkMovement(){
     for (let action in this.keybinds){
       for (let keyBind of this.keybinds[action]){
         if (keyIsDown(keyBind)){
-          this.move(action);
+          this.toMove.push(action);
+          break;
+        }
+      }
+    }
+
+    if (this.toMove.indexOf('left') !== -1 && this.toMove.indexOf('right') !== -1){
+      this.toMove.splice(this.toMove.indexOf('left'), 1);
+      this.toMove.splice(this.toMove.indexOf('right'), 1);
+    }
+
+    if (this.toMove.indexOf('up') !== -1 && this.toMove.indexOf('down') !== -1){
+      this.toMove.splice(this.toMove.indexOf('up'), 1);
+      this.toMove.splice(this.toMove.indexOf('down'), 1);
+    }
+  }
+
+  checkAbility(){
+    for (let action in this.specialKeys){
+      for (let keyBind of this.specialKeys[action]){
+        if (key === keyBind){
+          if (action === 'dash' && this.dashes > 0){this.triggerDash = true; this.dash();}
+          else if (action === 'jump' /*&& this.grounded*/){this.triggerjump = true; this.jump();}
           break;
         }
       }
@@ -76,25 +107,33 @@ class Player{
   }
 
   dash(){
+    print('dash');
     this.dashing = true;
     this.dashes--;
     this.movespeed = this.regularMovespeed*30;
     
     if (this.stationary){
-      if (this.facing === 0){this.vel.x -= this.movespeed;}
-      else {this.vel.x += this.movespeed;}
+      if (this.facing === 0){this.moveVel.x -= this.movespeed;}
+      else {this.moveVel.x += this.movespeed;}
     }
   }
 
-  move(action){
-    if (action === 'up') {this.vel.y -= this.movespeed;}
-    if (action === 'down') {this.vel.y += this.movespeed;}
-	  if (action === 'left') {this.vel.x -= this.movespeed; this.facing = 0;}
-    if (action === 'right') {this.vel.x += this.movespeed; this.facing = 1;}
+  jump(){
+    print('jump');
+    this.natural.y -= 7;
+  }
+
+  move(){
+    for (let action of this.toMove){
+      if (action === 'right') {this.moveVel.x += this.movespeed; this.facing = 1;}
+      else if (action === 'left') {this.moveVel.x -= this.movespeed; this.facing = 0;}
+      else if (action === 'up' && this.triggerDash) {this.moveVel.y -= this.movespeed;}
+      else if (action === 'down' && this.triggerDash) {this.moveVel.y += this.movespeed;}
+    }
   }
 
   modifyPosition(){
-    this.realPos.add(this.vel);
+    this.realPos.add(this.moveVel).add(this.naturalVel);
 
     this.oldPos.x = this.pos.x;
     this.oldPos.y = this.pos.y;
@@ -104,29 +143,30 @@ class Player{
   }
 
   update(){
-    this.modifyPosition()
+    this.checkMovement();
+    this.move();
+    this.modifyPosition();
 
     if (!this.dashing){
-      player.determineMovement();
-      if (abs(this.vel.x) > 0.03){this.vel.x *= 0.5;}
-      else{this.vel.x = 0;}
-      if (abs(this.vel.y) > 0.03){this.vel.y *= 0.5;}
-      else{this.vel.y = 0;}
+      if (abs(this.moveVel.x) > 0.03){this.moveVel.x *= 0.5;}
+      else{this.moveVel.x = 0;}
+      if (abs(this.moveVel.y) > 0.03){this.moveVel.y *= 0.5;}
+      else{this.moveVel.y = 0;}
     }
 
-    if (abs(this.vel.x) > 0 || abs(this.vel.y) > 0){this.stationary = false;}
+    if (abs(this.moveVel.x) > 0 || abs(this.moveVel.y) > 0){this.stationary = false;}
     else{this.stationary = true;}
 
-    this.dashes += 1/120
-
+    this.triggerDash = false;
     this.dashing = false;
+    this.toMove = [];
     this.movespeed = this.regularMovespeed;
   }
 
   display(){
     noStroke();
     fill(100, 200, 100);
-    rect(this.pos.x, this.pos.y, 9, 17);
+    rect(this.realPos.x, this.realPos.y, 9, 17);
   }
 }
 
@@ -160,6 +200,7 @@ class NeutralTerrain extends Terrain{
 
   onCollision(){
     print('Collision');
+    player.dashes = 2;
     if(false){}
   }
 }
